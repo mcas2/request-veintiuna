79ba08ec9d557fb5145527bf4f2ba0738216e624
mcas2 <mcas2@email.com>
2024-05-17 11:05:04 +0200

    Repartir cartas al jugador

--- veintiuna.js
+++ veintiuna.js
@@ -1,7 +1,22 @@
 let baraja = llenarBaraja()
-console.log(baraja)
 baraja = barajar(baraja)
-console.log(baraja);
+//console.log('Baraja recién barajada => ', baraja);
+
+const manoJugador = []
+repartir(2, manoJugador)
+console.log('Mano del jugador => ', manoJugador)
+//console.log('Baraja tras repartir al jugador => ',baraja);
+
+function jugar() {
+    console.log('¿Quieres otra?');
+    
+}
+
+function repartir(numCartas, mano) {
+    for (let i = 0; i < numCartas; i++) {
+        mano.push(baraja.pop())
+    }
+}
 
 function barajar(baraja) {
     return baraja.sort(function(a, b){return 0.5 - Math.random()})
